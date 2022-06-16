7610eea31a3548e91db01987f1502ffc23873be1
sidv23 <sidv23@gmail.com>
2022-06-16 15:10:07 -0400

    tmux conf

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,17 +1,3 @@
-# #using OhMyREPL
-
-atreplinit() do repl
-    try
-        @eval using OhMyREPL
-        @eval OhMyREPL.input_prompt!("Julia " * string(VERSION) * "> ", :green)
-        @eval colorscheme!("OneDark")
-    catch e
-        @warn "error while importing OhMyREPL" e
-    end
-
-    @async begin
-    # reinstall keybindings to work around https://github.com/KristofferC/OhMyREPL.jl/issues/166
-    sleep(1)
-    OhMyREPL.Prompt.insert_keybindings()
-    end
-end
+# using OhMyREPL
+# using GR
+# iterm() = inline("iterm")
